28f5db61dd683ff305d2d8fb88efc6db8b04e675
moodschoolproject <106934087+moodschoolproject@users.noreply.github.com>
2022-06-21 21:32:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ btn.addEventListener("click", () => {
 	let text = input.value;
 	console.log(text);
 	if (codes.includes(text)) {
-		res.innerHTML = '<a href="https://sites.google.com/learn.pearlriver.org/moodworldpremium/home">Click here!</a>'
+		res.innerHTML = '<a target="_blank" rel="noopener noreferrer"  href="https://sites.google.com/learn.pearlriver.org/moodworldpremium/home">Click here!</a>'
 	} else {
 		alert("Invalid code");
 	}
